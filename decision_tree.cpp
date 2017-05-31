680c35de29785e831ee0d598adc8efed45607da9
sandipan <sikdarsandipan99@gmail.com>
2017-05-31 11:43:03 +0530

    commit

---
+++ decision_tree.cpp
@@ -0,0 +1,22 @@
+#include<stdlib.h>
+#include<time.h>
+#include<iostream>
+#include<vector>
+#include<math.h>
+#include<map>
+#include<fstream>
+#include<string>
+#include<algorithm>
+#include "stat.h"
+/**
+
+implements basic decision tree classification agorithm
+input: feature matrix and class
+output: accuracy
+@author Sandipan Sikdar
+
+**/
+
+int main(int argc, char *argv[]){
+	return 0;
+}
