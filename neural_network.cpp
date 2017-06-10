7055f5f74dd3c96ad28294b369e1e1385f2319b4
sandipan <sikdarsandipan99@gmail.com>
2017-06-10 11:53:24 +0530

    commit

---
+++ neural_network.cpp
@@ -0,0 +1,23 @@
+#include<stdlib.h>
+#include<time.h>
+#include<iostream>
+#include<vector>
+#include<math.h>
+#include<map>
+#include<fstream>
+#include<string>
+#include<algorithm>
+#include "stat.h"
+/**
+
+implements basic 3 layer neural network
+input: feature matrix and class
+output: accuracy
+@author Sandipan Sikdar
+
+**/
+
+
+int main(int argc, char *argv[]){
+	return 0;
+}
