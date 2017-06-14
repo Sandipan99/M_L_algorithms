353204b436a3d679504d84136878b7433149ce19
sandipan <sikdarsandipan99@gmail.com>
2017-06-14 16:00:45 +0530

    commit

---
+++ logistic_regression.cpp
@@ -0,0 +1,22 @@
+#include<stdlib.h>
+#include<time.h>
+#include<iostream>
+#include<vector>
+#include<math.h>
+#include<fstream>
+#include<string>
+#include<algorithm>
+#include "stat.h"
+#include "common.h"
+/**
+
+implements logistic regression
+input: feature matrix and class
+output: accuracy
+@author Sandipan Sikdar
+
+**/
+
+int main(int argc, char *argv[]){
+	return 0;
+}
